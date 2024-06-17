aa946b375898ce4123b5f164def3f470b611e6c3
Cindy Chang <93894065+changyuhsin1999@users.noreply.github.com>
2024-06-16 18:00:32 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from llama_cpp import Llama
+
+# Initialize the Llama model
+llm = Llama(
+    model_path="./Phi-3-mini-4k-instruct-q4.gguf",  # path to GGUF file
+    n_ctx=4096,  # The max sequence length to use
+    n_threads=8, # The number of CPU threads to use
+    n_gpu_layers=35, # The number of layers to offload to GPU
+)
+
+def generate_response(prompt):
+    output = llm(
+        f"\n{prompt}\n",
+        max_tokens=500,  # Generate up to 256 tokens
+        stop=[""], 
+        echo=True,  # Whether to echo the prompt
+    )
+    return output['choices'][0]['text']
+
+# Streamlit app layout
+st.title("Travel assistant")
+
+st.write("Ask me anything!")
+
+# User input
+user_input = st.text_input("You:", "")
+
+# Generate response when user submits input
+if user_input:
+    with st.spinner("Thinking..."):
+        response = generate_response(user_input)
+    st.text_area("Llama:", response, height=200)
+
+# Run the app with streamlit
+#if __name__ == '__main__':
+    #st.run()
